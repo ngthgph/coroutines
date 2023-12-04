250cdc3674670810ba107cdfeb418fd98082f307
ngthgph <ngthgph112@gmail.com>
2023-12-05 01:07:28 +0700

    Using async, Deffered, await

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,23 +5,22 @@ fun main() {
     val time = measureTimeMillis {
         runBlocking {
             println("Weather forecast")
-            launch {
-                printForecast()
-            }
-            launch {
-                printTemperature()
-            }
+
+            val forecast: Deferred<String> = async { getForecast() }
+            val temperature: Deferred<String> = async { getTemperature() }
+
+            println("${forecast.await()} ${temperature.await()}")
             println("Have a good day!")
         }
     }
     println("Execution time: ${time/1000.0} seconds")
 }
-suspend fun printForecast() {
+suspend fun getForecast(): String {
     delay(1000)
-    println("Sunny")
+    return "Sunny"
 }
-suspend fun printTemperature() {
+suspend fun getTemperature(): String {
     delay(1000)
-    println("30\u00b0C")
+    return "30\u00b0C"
 }
 
