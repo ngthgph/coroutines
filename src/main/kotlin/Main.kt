35e61395b18895c1884d2ae126107baa81e6e754
ngthgph <ngthgph112@gmail.com>
2023-12-06 22:10:45 +0700

    Dispatchers - initial code

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -2,43 +2,11 @@ import kotlinx.coroutines.*
 import kotlin.system.*
 
 fun main() {
-    val time = measureTimeMillis {
-        runBlocking {
-            println("Weather forecast")
-            println(getWeatherReport())
-            println("Have a good day!")
+    runBlocking {
+        launch {
+            delay(1000)
+            println("10 results found")
         }
-
-//        // For test why have to use async, Deferred and await
-//        runBlocking {
-//            var fore = ""
-//            var temp = ""
-//            launch {
-//                fore = getForecast()
-//            }
-//            launch {
-//                temp = getTemperature()
-//            }
-//            println("$fore $temp")
-//        } => result nothing as fore, temp printed before get the value
+        println("Loading...")
     }
-    println("Execution time: ${time/1000.0} seconds")
 }
-suspend fun getWeatherReport() = coroutineScope {
-    val forecast = async { getForecast() }
-    val temperature = async { getTemperature() }
-
-    delay(2000)
-    temperature.cancel()
-
-    "${forecast.await()} "
-}
-suspend fun getForecast(): String {
-    delay(1000)
-    return "Sunny"
-}
-suspend fun getTemperature(): String {
-    delay(500)
-    return "30\u00b0C"
-}
-
